4a35194b73fa5ed8f1073f755a8f943fb5d2df96
Александр <116272931+Scorpic99@users.noreply.github.com>
2024-09-02 07:24:56 +0300

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,27 @@
+calls = 0
+
+
+def count_calls():
+    global calls
+    calls += 1
+
+
+def string_info(my_str):
+    count_calls()
+    my_tuple = (len(my_str))
+    return my_tuple, my_str.upper(), my_str.lower()
+
+
+def is_contains(string, list_to_search):
+    count_calls()
+    for i in list_to_search:
+        if i.lower() == string.lower():
+            return True
+    return False
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
+print(calls)
